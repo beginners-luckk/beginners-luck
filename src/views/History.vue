75b09cf03899ad67a3389ff314955e28556a128a
tootootooyaman <toyama.a.ab@gmail.com>
2021-09-08 19:49:12 +0900

    Historyの表形式の表示を修正

--- src/views/History.vue
+++ src/views/History.vue
@@ -2,35 +2,38 @@
   <div class="wrapper">
     <div style="margin: 20px">
       <pre>
-過去に受けた面接の履歴を表示しています。"GoToDetail"から詳細を確認できます！
-</pre
+過去に受けた面接の履歴を表示しています。"GoToDetail"から詳細を確認できます！</pre
       >
     </div>
-    <div id="app">
-      <div class="container">
-        <div class="row">
-          <div class="col-xs-12">
-            <div id="app">
-              <table class="table">
-                <thead>
-                  <tr>
-                    <p>No</p>
-                    <p>Mode</p>
-                    <p>Date</p>
-                    <p>録音を確認する</p>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-bind:key="item in items">
-                    <td>{{ item.no }}</td>
-                    <td>{{ item.mode }}</td>
-                    <td>{{ item.date }}</td>
-                    <td>{{ item.gotodetail }}</td>
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-          </div>
+    <link
+      rel="stylesheet"
+      href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+    />
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.6.1/css/all.css"
+    />
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>No</th>
+                <th>Mode</th>
+                <th>Date</th>
+                <th>録音を確認する</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr v-for="item in items" :key="item.no">
+                <td>{{ item.no }}</td>
+                <td>{{ item.mode }}</td>
+                <td>{{ item.date }}</td>
+                <td>{{ item.gotodetail }}</td>
+              </tr>
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
@@ -41,12 +44,14 @@
 export default {
   data() {
     return {
-      items: {
-        no: "example",
-        mode: "easy",
-        date: "2021/09/01",
-        gotodetail: "",
-      },
+      items: [
+        {
+          no: "example",
+          mode: "easy",
+          date: "2021/09/01",
+          gotodetail: "",
+        },
+      ],
     }
   },
 }
