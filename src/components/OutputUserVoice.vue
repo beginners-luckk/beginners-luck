0b2a85817b86afe6beb0392bc120681635670543
kreakdone <kreakdone@gmail.com>
2021-09-10 03:27:20 +0900

    firebaseを経由して入力音声を別画面で出力する機能の実装

--- src/components/OutputUserVoice.vue
+++ src/components/OutputUserVoice.vue
@@ -2,25 +2,63 @@
   <div>
     <div>
       <p>１番目</p>
-      <audio controls v-bind:src="voiceUrls[0]"></audio>
+      <audio controls v-bind:src="urls[0]"></audio>
     </div>
     <div>
       <p>２番目</p>
-      <audio controls v-bind:src="voiceUrls[1]"></audio>
+      <audio controls v-bind:src="urls[1]"></audio>
     </div>
-    <UserVoice :voiceUrls="voiceUrls" />
+    <button @click="getUrls">test</button>
   </div>
 </template>
 
 <script>
-import UserVoice from "@/components/userVoice.vue"
+import firebase from "firebase"
 
 export default {
-  components: { UserVoice },
   data() {
     return {
-      voiceUrls: [],
+      db: firebase.firestore().collection("users"),
+      urls: [],
+      uid: "",
     }
+  },
+  created() {
+    console.log("async")
+    const user = firebase.auth().currentUser
+    if (user !== null) {
+      // The user's ID, unique to the Firebase project. Do NOT use
+      // this value to authenticate with your backend server, if
+      // you have one. Use User.getToken() instead.
+      this.uid = user.uid
+      console.log(this.uid)
+    }
+    this.getUrls()
+  },
+  methods: {
+    getUrls: function () {
+      console.log("id=", this.uid)
+      console.log("getUrls")
+      const id = this.uid
+      const ref = this.db
+      ref
+        .where("uid", "==", id)
+        .get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach((doc) => {
+            // doc.data() is never undefined for query doc snapshots
+            console.log(doc.id, " => ", doc.data())
+            const data = doc.data()
+            data.id = doc.id
+            console.log(data)
+            console.log(data.userUrl)
+            this.urls = data.userUrl
+          })
+        })
+        .catch((error) => {
+          console.log("Error getting documents: ", error)
+        })
+    },
   },
 }
 </script>
