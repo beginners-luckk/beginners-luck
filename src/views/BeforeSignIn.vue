090ea4fffd5712cd38ba1e23ccd975e7a8f408c5
Yumeto3 <yumetouchida@gmail.com>
2021-09-11 11:33:11 +0900

    ドアgif妥協案

--- src/views/BeforeSignIn.vue
+++ src/views/BeforeSignIn.vue
@@ -3,6 +3,13 @@
     <h1 class="title">Job</h1>
     <h1 class="title">interview</h1>
     <button class="login" @click="signIn">LOGIN</button>
+    <div>
+      <img
+        class="bg"
+        alt="bc-gif"
+        src="https://img.gifmagazine.net/gifmagazine/images/2998664/original.gif"
+      />
+    </div>
   </div>
 </template>
 <script>
@@ -17,5 +24,19 @@ export default {
 <style>
 .title {
   text-align: center;
+}
+.bg {
+  width: 50%;
+  height: 300%;
+  padding-top: 20px;
+}
+
+.title {
+  font-size: 50px;
+  font-family: cursive;
+}
+.login {
+  width: 100px;
+  height: 30px;
 }
 </style>
